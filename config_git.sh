4947fd24d29da78e28acc56295ccb5df638883dd
lorisugolini <loris.ugolini@gmail.com>
2016-04-14 12:29:53 +0000

    Initialize repository

---
+++ config_git.sh
@@ -0,0 +1,6 @@
+git config --global push.default matching
+git config --global alias.co checkout
+
+# loro dicono --global ... con local??
+git config --global user.name "lorisugolini"
+git config --global user.email loris.ugolini@gmail.com
